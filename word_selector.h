5351aeaf1ae7df8b3d75c38186e6e455c57009fe
cristian cojocaru <cristiancojocaru04@yahoo.com>
2024-05-29 20:32:50 +0300

    the main files for the C code

---
+++ word_selector.h
@@ -0,0 +1,6 @@
+#ifndef WORD_SELECTOR_H // Verificam daca WORD_SELECTOR_H nu a fost definit anterior pentru a evita dublura.
+#define WORD_SELECTOR_H // Definim WORD_SELECTOR_H pentru a marca inceputul protectiei impotriva includerii multiple.
+
+void select_words(int num_letters); // Declaratia functiei select_words(), care va fi definita in alt fisier sursa.
+
+#endif // Terminam protectia impotriva includerii multiple.
